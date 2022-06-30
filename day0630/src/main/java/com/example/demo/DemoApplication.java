6e5778183d57d94283730d0b6656799a441002c9
wiufixo <104910435+wiufixo@users.noreply.github.com>
2022-06-30 12:15:51 +0900

    commit2

--- day0630/src/main/java/com/example/demo/DemoApplication.java
+++ day0630/src/main/java/com/example/demo/DemoApplication.java
@@ -9,18 +9,30 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		
-		Car mycar = new Car();
-		mycar.move();
+//		Car mycar = new Car();
+//		mycar.move();
+//		
+//		new Moveable() {
+//			@Override
+//			public void move() {
+//				System.out.println("날아서 움직입니다!");
+//			}
+//		}.move();
 		
-		new Moveable() {
-			@Override
-			public void move() {
-				System.out.println("날아서 움직입니다!");
-			}
-		}.move();
+//		Math sub = new Math() {
+//			
+//			@Override
+//			public int calc(int a, int b) {
+//				return a - b;
+//			}
+//			
+//		};
 		
-		
-		
+		Math sub = (a,b)->{
+			return a - b;
+		};
+		Math multi = (a,b)->{return a * b;};
+		Math add = (a,b)->a + b;
 	}
 
 }
---
+++ day0630/src/main/java/com/example/demo/Math.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+@FunctionalInterface
+public interface Math {
+	
+	public int calc(int a, int b);
+}
